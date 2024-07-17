724a9e3de9f4f116d4cee18d9ecb660abfad1c25
CI-DEV <154627941+IlumCI@users.noreply.github.com>
2024-07-17 14:20:31 +0300

    Update commands.rs

--- server/src/commands.rs
+++ server/src/commands.rs
@@ -1,1 +1,14 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
+pub struct Command {
+    pub id: usize,
+    pub command: String,
+    pub args: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CommandResult {
+    pub id: usize,
+    pub result: String,
+}
