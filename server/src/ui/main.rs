09d7dd85cc7e35877714474ede0f8e169ce113b5
CI-DEV <154627941+IlumCI@users.noreply.github.com>
2024-07-17 14:32:46 +0300

    Update main.rs

--- server/src/ui/main.rs
+++ server/src/ui/main.rs
@@ -1,1 +1,7 @@
-
+#[tokio::main]
+async fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![execute_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
