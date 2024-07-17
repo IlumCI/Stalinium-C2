836348cfe137fa74e3d1216561d263a39f97b0fb
CI-DEV <154627941+IlumCI@users.noreply.github.com>
2024-07-17 14:33:13 +0300

    Update app.vue

--- server/src/ui/app.vue
+++ server/src/ui/app.vue
@@ -1,1 +1,25 @@
+<template>
+  <div>
+    <h1>C2 Server Admin Panel</h1>
+    <div v-for="command in commands" :key="command.id">
+      <p>{{ command.result }}</p>
+    </div>
+  </div>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      commands: [],
+    };
+  },
+  methods: {
+    executeCommand(command, args) {
+      this.$tauri.invoke('execute_command', { command, args }).then((result) => {
+        this.commands.push(result);
+      });
+    },
+  },
+};
+</script>
